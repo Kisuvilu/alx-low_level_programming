ef5210ffad3f6f79262c21c8d45c79f9da6a9c8d
kisuvilu <kisuvilu@gmail.com>
2023-10-19 13:18:39 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+size_t nodes_printed = 0;
+
+while (h)
+{
+if (!h->str)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+nodes_printed++;
+}   
+{    
+return (nodes_printed);
+}
+}
