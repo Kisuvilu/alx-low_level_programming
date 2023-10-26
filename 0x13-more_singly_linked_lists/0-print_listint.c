a987e478cdedfb8a1b7a4eddc5601d03c1925f8f
kisuvilu <kisuvilu@gmail.com>
2023-10-25 17:22:10 -0700

    initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
+
 /**
- * print_listint - prints the int data in a singly linked list
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * @h: is the head pointer to list
- * Return: the number of nodes in the list
+ * Return: number of nodes
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
--- 0x13-more_singly_linked_lists/README.md
+++
@@ -1,1 +0,0 @@
-This project is about linked list  manipulation in C programming which is a low level programming language.
